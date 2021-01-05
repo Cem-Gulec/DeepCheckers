44434bc40d1cae2572629b60cf8adcc3f7b5d113
muhammetseramet <seramet.muhammet@gmail.com>
2021-01-06 01:45:09 +0300

    Bitboard representation and ArrayGame

--- code/phase4/Board.py
+++ code/phase4/Board.py
@@ -1,4 +1,26 @@
 
+# Oyunun kuralları :
+
+# Toplamda 5 tane bitboard olacak, bunlar : 
+# Beyeaz Pawn   : Normal ilerleyen taşlar
+# Beyaz King    : Bunlar Dama olmuş beyaz taşlar
+# Siyah Pawn    : Normal ilerleyen taşlar
+# Siyah King    : Bunlar Dama olmuş siyah taşlar   
+# Büyük Board   : Bunların hepsini içeren oyunun oynandığı board
+
+# Side == 1     : Sıra Beyazda (Player1),
+# Side == -1    : Sıra Siyahta (Player2);
+
+# Occupancies ile alakalı 3 board olacak:
+# Beyaz occupancy   : Bu bitboardda Beyaz taşların nerede olduğu 
+# Siyah occupancy   : Bu bitboardda Siyah taşların nerede olduğu 
+# All occupancy     : Bu bitboardda bütün taşların nerede olduğu 
+
+
+import logging
+logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
+
+
 class Board():
     
     # Türk daması board initial state
@@ -6,6 +28,78 @@ class Board():
 
     # saldırı hamleleri üzerinde denemek için
     __tmp_bitboard = 0x0000000000000000
+    
+                        # Tahtanın Represent Edilmesi İçin gerekli Bitboardlar
+    
+    #  Beyaz Pawn Taşları 'P' Tahtası                       Beyazın b8 de dama taşı olduğunu varsayarsak 'K' tahtasının son hali
+    #   8  0 0 0 0 0 0 0 0                                                  8  0 1 0 0 0 0 0 0    
+    #   7  0 0 0 0 0 0 0 0                                                  7  0 0 0 0 0 0 0 0  
+    #   6  0 0 0 0 0 0 0 0                                                  6  0 0 0 0 0 0 0 0  
+    #   5  0 0 0 0 0 0 0 0                                                  5  0 0 0 0 0 0 0 0  
+    #   4  0 0 0 0 0 0 0 0                                                  4  0 0 0 0 0 0 0 0  
+    #   3  1 1 1 1 1 1 1 1                                                  3  0 0 0 0 0 0 0 0  
+    #   2  1 1 1 1 1 1 1 1                                                  2  0 0 0 0 0 0 0 0  
+    #   1  0 0 0 0 0 0 0 0                                                  1  0 0 0 0 0 0 0 0  
+    #      a b c d e f g h                                                     a b c d e f g h
+    #   HEX : 0x0000000000ffff00
+    #
+    #  Siyah Pawn Taşları 'p' Tahtası                       Siyahın d4 de dama taşı olduğunu varsayarsak 'k' tahtasının son hali
+    #   8  0 0 0 0 0 0 0 0                                                  8  0 0 0 0 0 0 0 0    
+    #   7  1 1 1 1 1 1 1 1                                                  7  0 0 0 0 0 0 0 0  
+    #   6  1 1 1 1 1 1 1 1                                                  6  0 0 0 0 0 0 0 0  
+    #   5  0 0 0 0 0 0 0 0                                                  5  0 0 0 0 0 0 0 0  
+    #   4  0 0 0 0 0 0 0 0                                                  4  0 0 0 1 0 0 0 0  
+    #   3  0 0 0 0 0 0 0 0                                                  3  0 0 0 0 0 0 0 0  
+    #   2  0 0 0 0 0 0 0 0                                                  2  0 0 0 0 0 0 0 0  
+    #   1  0 0 0 0 0 0 0 0                                                  1  0 0 0 0 0 0 0 0  
+    #      a b c d e f g h                                                     a b c d e f g h
+    #    HEX = 0x00ffff0000000000
+    
+    __Pboard = 0x0000000000ffff00   # Beyaz pawn taşları initial state
+    __Kboard = 0x0000000000000000   # Beyaz dama taşları initial state
+    __pboard = 0x00ffff0000000000   # Siyah pawn taşları initial state
+    __kboard = 0x0000000000000000   # Siyah dama taşları initial state
+    
+    __allbitboards = [
+    __Pboard,              # Burası beyaz piyonları temsil etmekte, 
+    __Kboard,              # Burası beyaz dama taşlarını temsil ediyo,  
+    __pboard,              # Burası siyah piyonları temsil etmekte, 
+    __kboard               # Burası siyah dama taşlarını temsil ediyo
+    ]
+    
+    # Boardın tamamındaki toplam taşların gösterilmesi durumu
+    
+    #       Beyaz Taşlarının ilk konumda olduklarını tek farkının b4 de dama taşının olduğunu varsayalım,
+    #                       White Occupancies Tahtası Reprensente edilmiş hali
+    #
+    #                           8  0 0 0 0 0 0 0 0  
+    #                           7  0 0 0 0 0 0 0 0    
+    #                           6  0 0 0 0 0 0 0 0   
+    #                           5  0 0 0 0 0 0 0 0   
+    #                           4  0 1 0 0 0 0 0 0    
+    #                           3  1 0 1 1 1 1 1 1    
+    #                           2  1 1 1 1 1 1 1 1    
+    #                           1  0 0 0 0 0 0 0 0  
+    #                              a b c d e f g h
+    # 
+    #       Aynı mantıkla siyahın bütün taşlarını barındıran bir board 'black',
+    #       Hem siyahın tüm taşlarını hemde beyazın tüm taşlarını barındıran board 'both'
+    
+    __whiteocc  = 0x0000000000ffff00    # Beyazın tüm taşları initial statedeyken
+    __blackocc  = 0x00ffff0000000000    # Siyahın tüm taşları initial statedeyken
+    __bothocc   = 0x00ffff0000ffff00    # İkisininde tüm taşları initial statedeyken
+    
+    __occupancyboards = [
+    __whiteocc,
+    __blackocc,
+    __bothocc
+    ]
+    
+    # Oynama sırasının kimde olduğunu tutacak variable
+    #
+    #   side == 1   ==> Oynama sırası Beyaz taşların
+    #   side == 0   ==> Oynama sırası Siyah taşların
+    __side = 1
 
     # sayı-harf pozisyon labelları
     positions = [
@@ -43,16 +137,13 @@ class Board():
     
     def __init__(self):
         
-        # Bunlar genel olarak kullanılabilir, dursun şurada
-        self.BP = 0x00ffff0000000000    # Normal Siyah taşlar
-        self.BK = 0x0                   # Black Dama Olmuş Taşları 
-        self.WP = 0xffff00              # Normal Beyaz taşlar
-        self.WK = 0x0                   # White Dama Olmuş Taşları
+        self.ascii_pieces = ["P","K","p","k"]
         
     # Şimdilik o square değeri yerine orda belli bir taş varsa 1 dön, yoksa 0
     # ilerideki aşamalarda bunu taşın değeri olarak döndürülebilir
-    def get_square(self, index):
-        return 1 if (self.__bitboard & (0x1 << index)) else 0
+    
+    def get_square(self, bitboard, index):
+        return 1 if (bitboard & (0x1 << index)) else 0
     
     # girilen labela göre pozisyon indexini döndürür
     # belki set olayını yaparken işleri kolaylaştırır
@@ -60,19 +151,20 @@ class Board():
         return self.positions.index(label)
 
     # Belirlenen square taş koy
-    def set_square(self, index):
-        self.__bitboard |= (0x1 << index)
+    def set_square(self, bitboard, index):
+        bitboard |= (0x1 << index)
+        return bitboard
 
     # Saldırı hamlesi için belirtilen pozisyonları
     # boş tahtada set etmek için
-    def set_square_atk(self, index):
-        self.__tmp_bitboard |= (0x1 << index)
+    def set_square_atk(self, bitboard, index):
+        bitboard |= (0x1 << index)
 
     # Belirlenen squaredeki taşı sil
-    def pop_square(self, index):
+    def pop_square(self, bitboard, index):
         # Taşın olup olmadığını konrol et
-        if self.get_square(index):
-            self.__bitboard ^= (0x1 << index)
+        if self.get_square(bitboard, index):
+            bitboard ^= (0x1 << index)
         else: 0
 
     # Look-up table'i generate ettiğimiz hamlelerle doldurmak için
@@ -113,28 +205,28 @@ class Board():
 
     # Verilen indexte hareket ettirebileceğimiz taş
     # olduğunu farz ediyorum
-    def forward_move(self, side, index):
+    def forward_move(self, bitboard, side, index):
         # Beyaz hamle
         if (not side):
             move = index + 8
             # Eğer önünde taş yoksa ileri hareket edecek
-            if not self.get_square(move):
-                board.set_square(move)
-                board.pop_square(index)
+            if not self.get_square(bitboard, move):
+                board.set_square(bitboard, move)
+                board.pop_square(bitboard, index)
             else:
                 print("not a valid move")
 
         # Siyah hamle
         else:
             move = index - 8;
-            if not self.get_square(move):
-                board.set_square(move)
-                board.pop_square(index)
+            if not self.get_square(bitboard, move):
+                board.set_square(bitboard, move)
+                board.pop_square(bitboard, index)
             else:
                 print("not a valid move")
 
     # Asıl bitboard'un nasıl gözüktüğünü print etmek için
-    def print_bitboard(self):
+    def print_bitboard(self, bitboard):
         
         print("\n")
         
@@ -148,7 +240,7 @@ class Board():
                 # Square'in locationu, bit olarak hesaplanıyor
                 square = i * 8 + j
                 # Square'de taş varsa 1, yoksa 0 yaz
-                print(1 if (self.__bitboard & (0x1 << square)) else 0, end = " ")  
+                print(1 if (bitboard & (0x1 << square)) else 0, end = " ")  
     
             print("\n")
         print("   a b c d e f g h\n\n")
@@ -173,19 +265,54 @@ class Board():
     
             print("\n")
         print("   a b c d e f g h\n\n")
-
+    
+    def print_board(self):
+        print("\n")
+        
+        self.__Pboard = self.set_square(self.__Pboard, self.get_enum_index("e5"))
+        self.__allbitboards[0] = self.__Pboard
+        self.print_bitboard(self.__Pboard)
+        self.print_bitboard(self.__allbitboards[0])
+        print(self.get_square(self.__allbitboards[0], self.get_enum_index("e5")))#bitboard & (0x1 << index)
+        #TODO: Burada __allbitboards güncellenmiyor, sorunu bulmak lazım
+        
+        for i in reversed(range(8)):
+            for j in range(8):
+                
+                # SOl taraftaki board indexlerini yaz
+                if not j:
+                    print(i+1, end = "  ")
+                
+                # Square'in locationu, bit olarak hesaplanıyor
+                square = i * 8 + j
+                
+                piece = -1
+                
+                for k in range(4):
+                    if (self.get_square(self.__allbitboards[k], square)):
+                        piece = k
+                
+                print(self.ascii_pieces[piece] if piece != -1 else ".", end=" ")
+                
+                
+            print("\n")
+        print("   a b c d e f g h\n\n")
     
 
        
 
-if __name__ == "__main__":        
-    board = Board()
-    board.print_bitboard()
-    index_deneme = board.get_enum_index("h6")
+
+board = Board()
+board.print_board()
+
+#if __name__ == "__main__":        
+#    board = Board()
+#    board.print_bitboard()
+#    index_deneme = board.get_enum_index("h6")
     #board.set_square(index_deneme)
     #board.pop_square(index_deneme)
     # Beyaz a5 saldırıyor. Buna göre saldırabileceği pozisyonlar
     #board.print_attack_OnBoard(board.generate_pawn_attack(0, index_deneme))
 
-    board.forward_move(1, index_deneme)
-    board.print_bitboard()
+#    board.forward_move(1, index_deneme)
+#    board.print_bitboard()
---
+++ code/phase4/arraygame/CheckersLogic.py
@@ -0,0 +1,133 @@
+
+# Sol taraf row, Sağ taraf column gösterecek şekilde tasarlandı
+
+# Taşların değerleriyse şöyle:
+# 3     : Dama olmuş Beyaz renkli taş
+# 1     : Normal Beyaz renkli taş
+# 0     : Boş alan
+# -1    : Normal Siyah renkli taş
+# -3    : Dama olmuş Siyah renkli taş
+class Board():
+    
+    # Hamlelerimizin yönlerinin tutulduğu yer
+    #
+    #  1, 0     ==>  Alt tarafa doğru, aşağıya hamle yönü
+    # -1, 0     ==>  Üst tarafa doğru, yukarıya hamle yönü
+    #  0, 1     ==>  Yan tarafa doğru, sağ tarafa hamsle yönü
+    #  0, -1    ==>  Yan tarafa doğru, sol tarafa hamle yönü
+    __directions = [(-1,-1),(1,1),(0,1),(1,0)]
+    
+    # N burada boardın size'ını belirlemek için, default olarak 8
+    def __init__(self, n=8):
+        
+        self.n = n
+        
+        # Board kurulumu 
+        self.pieces = [None] * self.n
+        for i in range(self.n):
+            self.pieces[i] = [0] * self.n
+        
+        # Artık elimizde self.pieces 'de 8x8lik boş bir board var
+        
+        #               Initial case:
+        #       8 . . . . . . . .
+        #       7 S S S S S S S S
+        #       6 S S S S S S S S
+        #       5 . . . . . . . .
+        #       4 . . . . . . . .
+        #       3 B B B B B B B B
+        #       2 B B B B B B B B
+        #       1 . . . . . . . .
+        #         a b c d e f g h
+        
+        # Beyaz taşlar a2:h2 + a3:h3; Tahtada B ile gösterilen yerler
+        # Siyah taşlar a6:h6 + a7:h7; Tahtada S ile gösterilen yerler
+        
+        # Siyah taşlar
+        self.pieces[1] = [-1] * self.n
+        self.pieces[2] = [-1] * self.n
+        
+        # Beyaz taşlar
+        self.pieces[5] = [1] * self.n
+        self.pieces[6] = [1] * self.n
+    
+    def __getitem__(self, index): 
+        return self.pieces[index]
+    
+    def countDiff(self, color):
+        #  1    == Beyaz
+        #  0    == Boşluk
+        # -1    == Siyah
+        count = 0
+        for x in range(self.n):
+            for y in range(self.n):
+                if self[x][y] == color:
+                    count += 1
+                if self[x][y] == -color:
+                    count -= 1
+        
+        return count
+    
+    def get_legal_moves(self, color):
+        # returns all the legal moves
+        
+        return
+    def has_legal_moves(self, color):
+        return False
+    
+    def get_moves_for_square(self, square):
+    
+    def execute_move(self, move, color):
+        """Perform the given move on the board; flips pieces as necessary.
+        color gives the color pf the piece to play (1=white,-1=black)
+        """
+
+        #Much like move generation, start at the new piece's square and
+        #follow it on all 8 directions to look for a piece allowing flipping.
+
+        # Add the piece to the empty square.
+        # print(move)
+        
+        
+
+    def _discover_move(self, origin, direction):
+        """ Returns the endpoint for a legal move, starting at the given origin,
+        moving by the given increment."""
+
+    def _get_flips(self, origin, direction, color):
+        """ Gets the list of flips for a vertex and direction to use with the
+        execute_move function """
+        #initialize variables
+        flips = [origin]
+
+        for x, y in Board._increment_move(origin, direction, self.n):
+            #print(x,y)
+            if self[x][y] == 0:
+                return []
+            if self[x][y] == -color:
+                flips.append((x, y))
+            elif self[x][y] == color and len(flips) > 0:
+                #print(flips)
+                return flips
+
+        return []
+
+    @staticmethod
+    def _increment_move(move, direction, n):
+        # print(move)
+        """ Generator expression for incrementing moves """
+        move = list(map(sum, zip(move, direction)))
+        #move = (move[0]+direction[0], move[1]+direction[1])
+        while all(map(lambda x: 0 <= x < n, move)): 
+        #while 0<=move[0] and move[0]<n and 0<=move[1] and move[1]<n:
+            yield move
+            move=list(map(sum,zip(move,direction)))
+            #move = (move[0]+direction[0],move[1]+direction[1])
+
+    
+board = Board(8)
+for row in board:
+    print(row)
+        
+        
+        
