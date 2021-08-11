a34ef67286580de9f80920e87962ba857ba8ec3e
Cem Güleç <35612479+Cem-Gulec@users.noreply.github.com>
2021-08-11 14:11:13 +0300

    tile and board created

---
+++ code/phase5/checkers_ui/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Checkersboard from './components/Checkersboard';
+
+function App() {
+  return (
+    <div id="app">
+      <Checkersboard/>
+    </div>
+  );
+}
+
+export default App;
