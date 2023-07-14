f53e3e1a32051c4f3fbee69cba0fda2af358cdd2
cemgulec <cem.ggulecc@gmail.com>
2023-07-14 17:22:56 +0200

    zobrist hashing logic applied to mcts

--- code/phase5/MCTS.py
+++ code/phase5/MCTS.py
@@ -1,6 +1,6 @@
 import logging
 import math
-
+import random
 import numpy as np
 
 EPS = 1e-8
@@ -25,17 +25,47 @@ class MCTS():
         self.Es = {}  # stores game.getGameEnded ended for board s
         self.Vs = {}  # stores game.getValidMoves for board s
 
-    def getActionProb(self, canonicalBoard, temp=1):
+        self.zobTable = [[[random.randint(1,2**64 - 1) for i in range(4)]for j in range(4)]for k in range(8)]
+        self.hashValue = 0
+        self.hash_list = []
+        self.totalNodeCount = 0
+        self.repNodeCount = 0
+
+    # Zobrsit table'da hangi piece kaçıncı indexte bilgisini bulmak için
+    def indexing(self, piece):
+        if (piece == -1):   # Siyah taşlar index0 de tutuluyor
+            return 0
+        if (piece == 1):    # Beyaz taşlar index1 de tutuluyor
+            return 1
+        if (piece == -3):   # Siyah dama taşları index2 de tutuluyor
+            return 2
+        if (piece == 3):    # Beyaz dama taşları index3 de tutuluyor
+            return 3
+        else:
+            log.error("While indexing somethings went terribly wrong.")
+            log.info("The piece is : %s", str(piece))
+            return -1       # Test amaçlı?
+            
+    def computeHash(self, board):
+        h = 0
+        for i in range(8):
+            for j in range(4):
+                if board[i][j] != 0:
+                    piece = self.indexing(board[i][j])
+                    h ^= self.zobTable[i][j][piece]
+        return h
+    
+    def getActionProb(self, canonicalBoard, temp):
         """
         This function performs numMCTSSims simulations of MCTS starting from
         canonicalBoard.
-
         Returns:
             probs: a policy vector where the probability of the ith action is
                    proportional to Nsa[(s,a)]**(1./temp)
         """
         for i in range(self.args.numMCTSSims):
             self.search(canonicalBoard)
+            self.hash_list.clear()
 
         s = self.game.stringRepresentation(canonicalBoard)
         counts = [self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in range(self.game.getActionSize())]
@@ -49,6 +79,8 @@ class MCTS():
 
         counts = [x ** (1. / temp) for x in counts]
         counts_sum = float(sum(counts))
+        if counts_sum == 0:
+            self.game.display(canonicalBoard)
         probs = [x / counts_sum for x in counts]
         return probs
 
@@ -57,34 +89,42 @@ class MCTS():
         This function performs one iteration of MCTS. It is recursively called
         till a leaf node is found. The action chosen at each node is one that
         has the maximum upper confidence bound as in the paper.
-
         Once a leaf node is found, the neural network is called to return an
         initial policy P and a value v for the state. This value is propagated
         up the search path. In case the leaf node is a terminal state, the
         outcome is propagated up the search path. The values of Ns, Nsa, Qsa are
         updated.
-
         NOTE: the return values are the negative of the value of the current
         state. This is done since v is in [-1,1] and if v is the value of a
         state for the current player, then its value is -v for the other player.
-
         Returns:
             v: the negative of the value of the current canonicalBoard
         """
 
+        #print("MCTS.search: ", self.depth, " size of map: ", len(self.Ps))
         s = self.game.stringRepresentation(canonicalBoard)
         #self.game.display(canonicalBoard)
 
-        if s not in self.Es:
+        self.hashValue = self.computeHash(canonicalBoard)
+       
+        # Starting node dışındakilerin tekrarlı olmayacağını farz ettim
+        if s not in self.Es:            
             self.Es[s] = self.game.getGameEnded(canonicalBoard, 1)
+
         if self.Es[s] != 0:
             # terminal node
             return -self.Es[s]
+        
+        if self.hashValue not in self.hash_list:
+            self.hash_list.append(self.hashValue)
+            #print(self.depth)
+            #self.game.display(zobrist_board_temp)
 
         if s not in self.Ps:
             # leaf node
             self.Ps[s], v = self.nnet.predict(canonicalBoard)
             valids = self.game.getValidMoves(canonicalBoard, 1)
+            non_zero = [i for i, e in enumerate(valids) if e != 0]
             self.Ps[s] = self.Ps[s] * valids  # masking invalid moves
             sum_Ps_s = np.sum(self.Ps[s])
             if sum_Ps_s > 0:
@@ -102,6 +142,7 @@ class MCTS():
             self.Ns[s] = 0
             return -v
 
+        self.totalNodeCount += 1
         valids = self.Vs[s]
         cur_best = -float('inf')
         best_act = -1
@@ -120,18 +161,52 @@ class MCTS():
                     best_act = a
 
         a = best_act
+
         next_s, next_player = self.game.getNextState(canonicalBoard, 1, a)
+
         next_s = self.game.getCanonicalForm(next_s, next_player)
 
-        v = self.search(next_s)
+        temp_hash_val = self.computeHash(next_s)
+        if temp_hash_val not in self.hash_list:
+            
+            v = self.search(next_s)
+            # Repetition olmadı
+            #if v:
+            if (s, a) in self.Qsa:
+                self.Qsa[(s, a)] = (self.Nsa[(s, a)] * self.Qsa[(s, a)] + v) / (self.Nsa[(s, a)] + 1)
+                self.Nsa[(s, a)] += 1
 
-        if (s, a) in self.Qsa:
-            self.Qsa[(s, a)] = (self.Nsa[(s, a)] * self.Qsa[(s, a)] + v) / (self.Nsa[(s, a)] + 1)
-            self.Nsa[(s, a)] += 1
+            else:
+                self.Qsa[(s, a)] = v
+                self.Nsa[(s, a)] = 1
 
+            self.Ns[s] += 1
+            #print("END OF MCTS.search: ", self.depth, " size of map: ", len(self.Ps))
+            return -v
         else:
-            self.Qsa[(s, a)] = v
-            self.Nsa[(s, a)] = 1
-
-        self.Ns[s] += 1
-        return -v
+            # TODO Hocanın bahsettiği şekilde bir sonraki en uygun hamleyi seç
+            # Burada bütün hamleler repetitive hamle olursa ne olacak? Olabilir, beraberlik dön
+            v = None
+            while v is None:
+                next_s_childs = [i for i, e in enumerate(self.game.getValidMoves(next_s, 1)) if e != 0]
+                self.repNodeCount += len(next_s_childs)
+                #log.info('Repetition found with action : %s', str((a)))
+                non_zero = [i for i, e in enumerate(self.Vs[s]) if e != 0]
+                #log.info("Numbers in valids are: {}".format(' '.join(map(str, non_zero))))
+                    
+                if a not in non_zero:
+                    self.game.display(canonicalBoard)
+                    log.error("Something terrible has happen")
+                    
+                self.Vs[s][a] = 0       # Valid move listesinden repetitive hamleyi kaldır
+                non_zero = [i for i, e in enumerate(self.Vs[s]) if e != 0]
+                #log.info("New nums in valids are: {}".format(' '.join(map(str, non_zero))))
+                # Bütün hamleler masklandı oyun berabere
+                if len(non_zero) == 0:
+                    self.game.display(canonicalBoard) 
+                    log.error("No valids move remaining..")
+                    return next_player
+                    
+                v = self.search(canonicalBoard) # Yeni valids ve Qsa değerleriyle uygun hamleyi bul
+            
+            return -v
